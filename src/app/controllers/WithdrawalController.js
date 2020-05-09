a2c49ae9109b01b33ff52af130bf60dc32438aad
Paulo Goncalves <paullogoncalves@gmail.com>
2020-05-08 22:32:15 -0300

    conclusao/withdrawalController

--- src/app/controllers/WithdrawalController.js
+++ src/app/controllers/WithdrawalController.js
@@ -1,3 +1,5 @@
+import { Op } from 'sequelize';
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
 import Order from '../models/Order';
 
 class WithdrawalController {
@@ -12,6 +14,25 @@ class WithdrawalController {
     const deliveryUpdate = await delivery.update({
       start_date: req.body.start_date,
     });
+
+    const data = await parseISO(req.body.start_date);
+    console.log('DATA:', data);
+    console.log('DATA:', startOfDay(data));
+    console.log('DATA:', endOfDay(data));
+    const qnt = await Order.count({
+      where: {
+        deliveryman_id: req.params.idDeliveryman,
+        start_date: {
+          [Op.between]: [startOfDay(data), endOfDay(data)],
+        },
+      },
+    });
+
+    if (qnt >= 5) {
+      return res
+        .status(401)
+        .json({ error: 'Withdrawal quantity can not greater than 5.' });
+    }
 
     return res.json(deliveryUpdate);
   }
